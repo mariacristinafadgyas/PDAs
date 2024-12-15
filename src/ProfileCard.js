3ff219b3cc2e8b4d89d0b4bcd1101981aa994d99
Maria Cristina Fadgyas <mariacristinafadgyas@Marias-Air.fritz.box>
2024-12-15 18:25:35 +0100

    added image logos

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,7 @@
-function ProfileCard(props) {
-    const title = props.title;
-    const handle = props.handle;
-
+function ProfileCard({title, handle, image }) {
     return (
         <div>
+            <img src={image} alt="pda logo"/>
             <div>Title is {title}.</div>
             <div>Handle is {handle}.</div>
         </div>
